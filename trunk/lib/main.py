2f35534f6b600cee5e654e826cedce7374f0d1bc
chinomng <chinomng@765bbe15-3f39-0410-8262-a377e201cfcf>
2007-09-05 03:05:26 +0000

    ordene el codigo de main. La explosion esta en una clase aparte

--- trunk/lib/main.py
+++ trunk/lib/main.py
@@ -8,6 +8,7 @@ import utils
 from hero import Hero
 from moldsmanager import MoldsManager
 from config import  *
+from explotion import Explotion
 
 if not pygame.font: print 'Warning, fonts disabled'
 if not pygame.mixer: print 'Warning, sound disabled'
@@ -23,17 +24,6 @@ class Level(object):
 
         self.background = utils.create_surface((width, height), (0,0,0))
         self.screen.blit(self.background, (0, 0))
-
-
-        
-        #self.screen.blit(self.background, (0,0))
-        #tit_color = (40,40,40)
-        #title_img = font_title.render('PRUEBA', True, tit_color)
-        #topleft = (background.get_rect().width - title_img.get_rect().width) / 2, 30
-        #bg = self.background.copy()
-        #bg.blit(title_img, topleft)
-        #self.background = bg        
-
         
         #Create the game clock
         self.clock = pygame.time.Clock()
@@ -71,52 +61,26 @@ class Level(object):
 
 
 
-
-        #Set blood effect
-        self.blood = pygame.sprite.RenderUpdates()
-        explotion = False	
-        blood_flag = 0 
-        for x in xrange(333):	
-            blood_drop = BloodDrop()
-            self.blood.add(blood_drop)
-
+        self.explotion = Explotion()
+	
         while self.time > 0:
             self.tics += 1            
             self.time = (self.max_time - self.tics) / self.max_time
-
-            
-            self.timeBar.update(self.time)
-            self.mm.gen(self.tics)
-            self.mm.move(self.tics)
-            
+            self.update()
             self.screen.blit(self.background, (0,0)) 
 
-
             #Blood Explotion
-            if explotion:
-                self.blood.update(self.tics)
-                self.blood.draw(self.screen)
-                blood_flag += 1             
-            
-            if blood_flag == 50:
-                explotion = False
+            self.explotion.update(self.tics, self.screen)
  
             #Verify collision
             if self.mm.fit(self.hero.group, self.tics):
-
                 self.points += 1
                 self.pointsCounter.update(self.points)
-
-                blood_flag = 0
-                explotion = True
+                self.explotion.boom(self.hero.parts['cheat'].rect)
                 self.pointsCounter.add_positive()
-                for blood_drop in self.blood:
-                    blood_drop.set_position(self.hero.parts['cheat'].rect.top, self.hero.parts['cheat'].rect.left)
 
-            #Draw
-            self.gadgets.draw(self.screen)
-            self.mm.draw(self.screen)
-            self.hero.group.draw(self.screen)
+
+            self.draw()
 
             #Control
             for event in pygame.event.get():
@@ -125,7 +89,15 @@ class Level(object):
             self.clock.tick(50)
             pygame.display.flip()
 
-
+    def update(self):
+        self.timeBar.update(self.time)
+        self.mm.gen(self.tics)
+        self.mm.move(self.tics)
+	
+    def draw(self):
+        self.gadgets.draw(self.screen)
+        self.mm.draw(self.screen)
+        self.hero.group.draw(self.screen)	
     def control(self, event):
         
         if event.type == QUIT:
