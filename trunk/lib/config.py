16d25cdc5de2b259f68c2ad3940411082048b727
jjconti <jjconti@765bbe15-3f39-0410-8262-a377e201cfcf>
2007-09-05 16:06:50 +0000

    Mejor sonido para la retorcida.

--- trunk/lib/config.py
+++ trunk/lib/config.py
@@ -26,6 +26,7 @@ BLOOP = os.path.join(SOUNDS, "bloop.wav")
 SCREAM1 = os.path.join(SOUNDS, "31818__malexmedia__malexmedia_man_scream.wav")
 SCREAM2 = os.path.join(SOUNDS, "34283__hello_flowers__Flowers_Like_to_Scream_03.wav")
 SCREAM3 = os.path.join(SOUNDS, "34292__hello_flowers__Flowers_Like_to_Scream_12.wav")
+CRACK1 = os.path.join(SOUNDS, "21914__Halleck__neck_crack.wav")
 
                      
 #FONT1 = os.path.join(FONTS,"FreeMonoBold.ttf")
