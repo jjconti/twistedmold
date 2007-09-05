d1b15f2fa06a2d8428a7fc0f2286eff6343e44b1
jjconti <jjconti@765bbe15-3f39-0410-8262-a377e201cfcf>
2007-09-05 16:06:50 +0000

    Mejor sonido para la retorcida.

--- lib/music.py
+++ lib/music.py
@@ -9,10 +9,11 @@ SOUNDS['eat'] = utils.load_sound(BLOOP)
 SOUNDS['scream1'] = utils.load_sound(SCREAM1)
 SOUNDS['scream2'] = utils.load_sound(SCREAM2)
 SOUNDS['scream3'] = utils.load_sound(SCREAM3)
-screems = [SOUNDS['scream1'], SOUNDS['scream2'], SOUNDS['scream3']]
+SOUNDS['crack1'] = utils.load_sound(CRACK1)
 
 def play_bloop():
     SOUNDS['eat'].play()
 
 def play_scream():
-    random.choice(screems).play()
+    SOUNDS['crack1'].play()
+
