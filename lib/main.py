53d114125abe23817652797a7bfcb20597eb2806
jjconti <jjconti@765bbe15-3f39-0410-8262-a377e201cfcf>
2007-09-06 04:50:49 +0000

    Estrcutura definida para los niveles y BUG FIXES.

--- lib/main.py
+++ lib/main.py
@@ -29,7 +29,7 @@ def menu(screen):
     return Menu(screen, options, WINDOW_TITLE)
 
 def play(screen):
-    return Level(screen, menu, energy_leap=.05, mold_velocity=10, mold_density=75)
+    return Level(screen, menu, 1, 0)
 
 def exit():
     sys.exit(0)
