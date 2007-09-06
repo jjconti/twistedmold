681d7bf2f7b393cbcf67da7b61461af6b097ad04
ceportela <ceportela@765bbe15-3f39-0410-8262-a377e201cfcf>
2007-09-06 13:57:34 +0000

    modificado main.py para integrar scores.py

--- trunk/lib/main.py
+++ trunk/lib/main.py
@@ -5,6 +5,7 @@ import utils
 from config import  *
 from level import Level
 from menu import Menu
+from scores import HighScores
 
 if not pygame.font: print 'Warning, fonts disabled'
 if not pygame.mixer: print 'Warning, sound disabled'
@@ -24,12 +25,15 @@ def main():
     opcion()        #Exit
 
 def menu(screen):
-    options = [("Play", play), ("Story", None), ("Setup", None), \
-               ("Help", None), ("Credits", None), ("Exit",  exit)]
+    options = [("Play", play), ("Setup", None), ("Help", None), \
+               ("High Scores", scores), ("Credits", None), ("Exit",  exit)]
     return Menu(screen, options, WINDOW_TITLE)
 
 def play(screen):
     return Level(screen, menu, 1, 0)
+
+def scores(screen):
+    return HighScores(screen,menu)
 
 def exit():
     sys.exit(0)
