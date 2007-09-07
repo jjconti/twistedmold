c3ec9caa3e1dc0a4fdc6f1dd92175dde18c66339
ceportela <ceportela@765bbe15-3f39-0410-8262-a377e201cfcf>
2007-09-07 22:28:22 +0000

    cambios en credits.py

--- lib/credits.py
+++ lib/credits.py
@@ -8,12 +8,16 @@ from config import *
 import utils
 import music
 
+from time import sleep
+
 class Credits(object):
 
     def __init__(self, screen, father=None):
         self.screen = screen
         self.father = father
         self.credits = []
+        self.font1 = pygame.font.Font(FONT3, 20)
+        self.font2 = pygame.font.Font(FONT3, 40)
 
     def loop(self):
         music.play_music(CREDITSMUSIC)
@@ -33,60 +37,79 @@ class Credits(object):
         except IOError:
             print 'Cannot open credits file'
 
-    def _convert(self):
-        for item in self.credits:
-            while len(item[0])<len(item[1]):
-                item[0]=" "+item[0]+" "
-            while len(item[1])>len(item[0]):
-                item[1]=" "+item[1]+" "
-        print self.credits    
-
     def _draw_screen(self):
-
         pygame.display.set_caption(WINDOW_TITLE)
         background = utils.load_image(CREDITIMAGE)
 
-        font1 = pygame.font.Font(FONT3, 20)
-        font2 = pygame.font.Font(FONT3, 40)
         clock = pygame.time.Clock()
 
         title = 'CREDITS'
-        title_img = font2.render(title, True, WHITE)
+        title_img = self.font2.render(title, True, WHITE)
         topleft = (background.get_rect().width - title_img.get_rect().width) / 2, 30
         background.blit(title_img, topleft)
  
         self.screen.blit(background, (0, 0))
         pygame.display.flip()
+        hor_step = self.font2.get_height()
 
-        done = False
-        timeloop = 0
-        state = 0
-        y1 = 420
-        y2 = 470    
+        for text_list in self.credits:
+            done = False
+            timeloop = 0
+            state = 0
 
-        w = self.screen.get_width()
-        
-        for item in self.credits:
-            img1 = font1.render(item[0], True, WHITE)
-            img2 = font2.render(item[1], True, WHITE)
-            self.screen.blit(background, (0, 0))
-            pygame.display.flip()
-            
-            for i in range(w+img2.get_width()):
-                x1 = i
-                x2 = w-i
-                if x1 == x2:
-                    pygame.time.delay(1)
-                x1 -= img1.get_width()/2
-                x2 -= img2.get_width()/2
-                if x1 == x2:
-                    pygame.time.delay(2)
-                self.screen.blit(img1, (x1, y1))
-                self.screen.blit(img2, (x2, y2))
+            while not done:
+                clock.tick(CLOCK_TICS)
                 pygame.display.flip()
                 self.screen.blit(background, (0, 0))
-                if pygame.event.peek([QUIT,K_RETURN,K_ESCAPE]):
-                    return self.father
+                y = hor_step + 380
+
+                timeloop += 1
+                if timeloop == CLOCK_TICS:
+                    state = 1
+                    done = True
+
+                for i,text_item in enumerate(text_list):
+                    img = self.font2.render(text_item, True, WHITE)
+                    x2 = self.screen.get_width()/2
+                    if (state == 0) and (i%2 == 0):
+                        x1 = x2 - ((WIDTH * 0.86) * (50 - timeloop) / 50)
+                    elif (state == 0) and (i%2 == 1):
+                        x1 = x2 + ((WIDTH * 0.86) * (50 - timeloop) / 50)
+                    else:
+                        x1=x2
+                    x = (x1+(x2-x1)*(1-math.exp(-timeloop/20.0)))
+                    x -= img.get_width()/2
+                    self.screen.blit(img, (x, y))
+                    y += hor_step + 10
+
+            pygame.time.delay(250)
+
+            done = False
+            timeloop = CLOCK_TICS
+            state = 1
+
+            while not done:
+                clock.tick(CLOCK_TICS)
+                pygame.display.flip()
+                self.screen.blit(background, (0, 0))
+                y = hor_step + 380
+
+                timeloop -= 1
+                if timeloop == 0:
+                    state = 0
+                    done = True
+
+                for i,text_item in enumerate(text_list):
+                    img = self.font2.render(text_item, True, WHITE)
+                    x2 = self.screen.get_width()/2
+                    if (i%2 == 0):
+                        x1 = (x2 + ((WIDTH * 0.86) * (50 - timeloop) / 50))
+                    elif (i%2 == 1):
+                        x1 = (x2 - ((WIDTH * 0.86) * (50 - timeloop) / 50))
+                    x = (x1+(x2-x1)*(1-math.exp(-timeloop/20.0)))
+                    x -= img.get_width()/2
+                    self.screen.blit(img, (x, y))
+                    y += hor_step + 10
 
     def _waitKey(self):
         while 1:
