d80bd8112845970f6d33685e56f948704a521ba2
chinomng <chinomng@765bbe15-3f39-0410-8262-a377e201cfcf>
2007-09-08 05:34:13 +0000

    nuevos parametros para nivel

--- lib/bottlemanager.py
+++ lib/bottlemanager.py
@@ -11,7 +11,7 @@ class BottleManager(object):
     bottle1 = dict(energy=10, rotation=15, image=BOTTLE_BLUE, velocity=10) # azul
     bottle2 = dict(energy=-10, rotation=5, image=BOTTLE_GREEN, velocity=6) # verde
     bottle3 = dict(energy=25, rotation=30, image=BOTTLE_ORANGE, velocity=20) # naranja
-    bottle4 = dict(energy=0, rotation=15, image=BOTTLE_RED, velocity=10, destroy_all=True) # roja
+    bottle4 = dict(energy=0, rotation=15, image=BOTTLE_RED, velocity=6, destroy_all=True) # roja
 
     def __init__(self, bottle1_density, bottle2_density, bottle3_density, bottle4_mount):
         self.bottles = pygame.sprite.Group()
@@ -43,21 +43,21 @@ class BottleManager(object):
         if random.randrange(self.bottle1_density) == 0 and self.bottle1_flag: 
             bot = Bottle(self.bottle1)
             self.bottles.add(bot)
-            self.bottle1_flag = CLOCK_TICS
+            self.bottle1_flag = CLOCK_TICS*2
         else:
             self.bottle1_flag -= 1
 
         if random.randrange(self.bottle2_density) == 0 and self.bottle1_flag: 
             bot = Bottle(self.bottle2)
             self.bottles.add(bot)
-            self.bottle2_flag = CLOCK_TICS
+            self.bottle2_flag = CLOCK_TICS*2
         else:
             self.bottle2_flag -= 1
 
         if random.randrange(self.bottle3_density) == 0 and self.bottle1_flag: 
             bot = Bottle(self.bottle3)
             self.bottles.add(bot)
-            self.bottle3_flag = CLOCK_TICS
+            self.bottle3_flag = CLOCK_TICS*2
         else:
             self.bottle3_flag -= 1
 
@@ -65,7 +65,7 @@ class BottleManager(object):
             bot = Bottle(self.bottle4)
             self.bottles.add(bot)
             self.bottle4_current_mount += 1
-            self.bottle2_flag = CLOCK_TICS
+            self.bottle2_flag = CLOCK_TICS*2
         else:
             self.bottle4_flag -= 1
             
