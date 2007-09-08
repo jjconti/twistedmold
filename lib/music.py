7a00a1406a6145a61c25553ae3364d8187f5d80a
jjconti <jjconti@765bbe15-3f39-0410-8262-a377e201cfcf>
2007-09-08 05:57:11 +0000

    borrado archivos de sonido

--- lib/music.py
+++ lib/music.py
@@ -8,7 +8,7 @@ pygame.mixer.init()
 SOUNDS = {}
 SOUNDS['eat'] = utils.load_sound(BLOOP)
 SOUNDS['scream1'] = utils.load_sound(SCREAM1)
-SOUNDS['scream2'] = utils.load_sound(SCREAM2)
+#SOUNDS['scream2'] = utils.load_sound(SCREAM2)
 SOUNDS['scream3'] = utils.load_sound(SCREAM3)
 SOUNDS['crack1'] = utils.load_sound(CRACK1)
 SOUNDS['gameover'] = utils.load_sound(GAMEOVERMUSIC)
