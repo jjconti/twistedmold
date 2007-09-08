07e88532e1d54b386a920a10ef90da5912eeea53
jjconti <jjconti@765bbe15-3f39-0410-8262-a377e201cfcf>
2007-09-08 02:40:44 +0000

    Imagen fonde creditos

--- lib/credits.py
+++ lib/credits.py
@@ -43,7 +43,7 @@ class Credits(object):
 
     def _draw_screen(self):
         pygame.display.set_caption(WINDOW_TITLE)
-        background = utils.load_image(MENUBGIMAGE)
+        background = utils.load_image(CREDITSIMAGE)
 
         clock = pygame.time.Clock()
         separator = 2
